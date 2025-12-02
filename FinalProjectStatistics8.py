e45224474fba59aee0eb84e2b952af1b31d80abb
zaharaamalia-sys <zahara.amalia@student.president.ac.id>
2025-12-02 14:12:05 +0700

    Update FinalProjectStatistics8.py

--- FinalProjectStatistics8.py
+++ FinalProjectStatistics8.py
@@ -5,7 +5,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from scipy import stats
 import matplotlib.pyplot as plt
 from fpdf import FPDF
@@ -630,3 +629,4 @@ elif page == T["team"]:
 # ---------------------------
 # End of file
 # ---------------------------
+
